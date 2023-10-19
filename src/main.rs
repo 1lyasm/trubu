391f9779d5fd1447cfd19beeaabeed81543e9f8d
1lyasm <il.mystafa@gmail.com>
2023-10-19 21:02:00 +0300

    remove newline from first line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let lines: Vec<String> = std::io::stdin().lines().collect::<Result<_, _>>().unwrap();
-    let shard_count = lines.get(0).unwrap().parse().unwrap();
+    let first_line = lines.get(0).unwrap();
+    let shard_count = first_line[0..first_line.len() - 1].parse().unwrap();
     let addresses = lines[1..lines.len()].to_vec();
     let client = dianadb::Client::connect(shard_count, &addresses).unwrap();
     let statement = "SELECT column_name FROM table_name".to_string();
